a8368b0d1dc2c7ad4626a676580e7a7c4a42c601
Souradip <dgsouro77@gmail.com>
2024-09-14 23:04:36 +0530

    Updated the data_loader and removed the images

--- student_resource_3/src/data_loader.py
+++ student_resource_3/src/data_loader.py
@@ -1,29 +1,62 @@
 import os
-from torchvision import transforms, datasets
-from torch.utils.data import DataLoader
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import numpy as np
+import pandas as pd
+from src.constants import allowed_units
 
-def get_transforms():
-    return transforms.Compose([
-        transforms.Resize((128, 128)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Optional: Normalize as per standard practice
-    ])
-
-def get_data_loaders(train_dir, val_dir=None, batch_size=32):
-    if not os.path.isdir(train_dir):
-        raise ValueError(f"Training directory {train_dir} does not exist")
+class ProductImageDataset(Dataset):
+    def __init__(self, df, img_dir, transform=None):
+        self.df = df
+        self.img_dir = img_dir
+        self.transform = transform
+        
+        # Remove rows where image files are missing
+        self.df = self.df[self.df['index'].apply(lambda x: os.path.exists(os.path.join(img_dir, f"{x}.jpg")))]
+        
+    def __len__(self):
+        return len(self.df)
     
-    transform = get_transforms()  # Use the get_transforms function for both training and validation datasets
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        img_name = os.path.join(self.img_dir, f"{row['index']}.jpg")
+        
+        image = Image.open(img_name).convert('RGB')
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        value = torch.tensor(float(row['value'])) if pd.notna(row['value']) else torch.tensor(0.0)
+        unit = torch.tensor(allowed_units.index(row['unit'])) if row['unit'] in allowed_units else torch.tensor(0)
+        
+        return image, value, unit
 
-    train_dataset = datasets.ImageFolder(root=train_dir, transform=transform)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+# Data augmentation and normalization for training
+train_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# Just normalization for validation
+val_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+def get_data_loaders(train_df, img_dir, batch_size=32, num_workers=2):
+    full_dataset = ProductImageDataset(train_df, img_dir, transform=train_transform)
+    train_size = int(0.8 * len(full_dataset))
+    val_size = len(full_dataset) - train_size
+    train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
     
-    val_loader = None
-    if val_dir and os.path.isdir(val_dir):
-        val_dataset = datasets.ImageFolder(root=val_dir, transform=transform)
-        val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
-
+    val_dataset.dataset.transform = val_transform
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    
     return train_loader, val_loader
-
-# Usage in the script
-train_loader, val_loader = get_data_loaders('D:/Amazon ML Challenge/66e31d6ee96cd_student_resource_3/student_resource_3/images/train_images')
